e1877f348109b82cf8d5e252bb3e0465b079c346
Jonathan "Duke" Leto <jonathan@leto.net>
2018-12-22 06:38:51 -0800

    Add debug env var to dump full data, default now is stats only

--- wack.pl
+++ wack.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use BerkeleyDB;
 
+my $DEBUG = $ENV{DEBUG} || 0;
 my %wallet;
 my $filename = shift || 'wallet.dat';
 my $db  = tie %wallet, 'BerkeleyDB::Btree',
@@ -19,28 +20,33 @@ while (my ($k,$v) = each %wallet) {
     my $key  = substr $k, $len+1;
 
     #printf "%s => %x\n", $k, $v;
-    if ($type eq 'key') {
-        my $privkey = unpack("H*", $v);
-        $key = unpack("H*", $key);
-        print "key=$key, privkey=$privkey\n";
-    } elsif ($type eq 'zkey') {
-        my $privkey = unpack("H*", $v);
-        $key = unpack("H*", $key);
-        print "zkey=$key, privkey=$privkey\n";
-    } elsif ($type eq 'name') {
-        print "name: $key, $v\n";
-    } elsif ($type eq 'bestblock') {
-        my $len = length $v;
-        print "bestblock ($len bytes):\nbestblock: $key, $v\n";
-    } elsif ($type eq 'version') {
-        my $version = unpack("I", $v);
-        print "version: $version\n";
-    } else {
-        my $len = length $v;
-        print "$type ($len bytes): $key, $v\n";
+    if ($DEBUG) {
+        if ($type eq 'key') {
+            my $privkey = unpack("H*", $v);
+            $key = unpack("H*", $key);
+            print "key=$key, privkey=$privkey\n";
+        } elsif ($type eq 'defaultkey') {
+            my $dkey = unpack("H*", $v);
+            print "defaultkey=$dkey\n";
+        } elsif ($type eq 'zkey') {
+            my $privkey = unpack("H*", $v);
+            $key = unpack("H*", $key);
+            print "zkey=$key, privkey=$privkey\n";
+        } elsif ($type eq 'name') {
+            print "name: $key, $v\n";
+        } elsif ($type eq 'bestblock') {
+            my $len = length $v;
+            print "bestblock ($len bytes):\nbestblock: $key, $v\n";
+        } elsif ($type eq 'version') {
+            my $version = unpack("I", $v);
+            print "version: $version\n";
+        } else {
+            my $len = length $v;
+            print "$type ($len bytes): $key, $v\n";
+        }
     }
     $counts->{$type}++;
-    printf "$len $type %s:\n", $key;
+    #printf "$len $type %s:\n", $key;
 }
 
 printf "Stats\n";
